bb41b611c5ad9918841f0519f786287953790a53
pink-27 <98738847+pink-27@users.noreply.github.com>
2022-04-08 15:31:08 +0530

    Update disc.py

--- disc.py
+++ disc.py
@@ -1,3 +1,15 @@
 import discord
-print('hi')
-print('world')
+import os
+client = discord.Client()
+@client.event
+async def on_ready():
+  print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+  if message.author==client.user:
+    return
+  if message.content.startswith('+hello'):
+    await message.channel.send('Sup shawty!')
+
+client.run(os.getenv('TOKEN'))
